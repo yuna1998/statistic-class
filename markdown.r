c79f05df58bd51696dbb8500cfa9b36cda7d516c
yuna1998 <80019899+yuna1998@users.noreply.github.com>
2021-03-11 14:37:12 +0900

    Colaboratory를 통해 생성됨

---
+++ markdown.r
@@ -0,0 +1,66 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "markdown.r",
+      "provenance": [],
+      "authorship_tag": "ABX9TyMC7x/iddiSuQxQoIEYnJA8",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "ir",
+      "display_name": "R"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/yuna1998/statistic-class/blob/main/markdown.r\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 157
+        },
+        "id": "ErNIRE0wxpEq",
+        "outputId": "d2ce4562-e69c-4cde-8775-ee47f60d7038"
+      },
+      "source": [
+        "'''{r   }\r\n",
+        "1 +1\r\n",
+        "'''"
+      ],
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "error",
+          "ename": "ERROR",
+          "evalue": "ignored",
+          "traceback": [
+            "Error in parse(text = x, srcfile = src): <text>:1:3: unexpected string constant\n2: 1 +1\n3: '\n     ^\nTraceback:\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "UmTNPPaUx-pn"
+      },
+      "source": [
+        ""
+      ],
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
