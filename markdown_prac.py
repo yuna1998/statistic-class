b98d2190494d2450de5d9d6f2ace5a6fbd73aed2
yuna1998 <26594542@naver.com>
2021-03-11 19:33:40 +0900

    update markdown_prac.ipynb file

---
+++ markdown_prac.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This is title
+
+#     - number 1
+#     - second
+#  
+# this is mathematical expression: $ \sum_{i=0}^n X_i $
+
+# $$
+# \sum_{i=1}^n\frac{1}{x_i}
+# $$
+
+# In[ ]:
+
+
+
+
